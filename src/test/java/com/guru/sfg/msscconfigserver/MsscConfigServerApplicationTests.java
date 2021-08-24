92e87a0534f38329466c7f4b6faa2a41d66a4161
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-08-24 12:02:09 +0200

    Initial project add

---
+++ src/test/java/com/guru/sfg/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.sfg.msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
